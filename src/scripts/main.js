00212e67dfc830b38e73251f885cd5d8882e2172
Diego Costa <diego@diegocosta.com.br>
2016-07-04 22:50:20 -0300

    Diversos ajustes na sessão sobre o evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
 (function($){
     $(document).on('ready', function(){
 
-        $('.sobre-o-evento--button-continue').on('click', function(event){
-            event.preventDefault();
+        $('.sobre-o-evento--texto p:nth-child(3), .sobre-o-evento--texto p:nth-child(4)').hide();
 
-            $('.sobre-o-evento--texto p').toggle();
+        $('.sobre-o-evento--button-continue').each(function(index, item){
+            $this = $(this);
+            $this.css('display', 'block');
+            $this.on('click', function(event){
+                event.preventDefault();
 
+                var btn = $(this);
+
+                if(btn.attr('data-page') == 1){
+                    btn.text("Voltar");
+                    btn.attr("data-page", 2);
+                }
+                else {
+                    btn.text("Continuar Lendo");
+                    btn.attr("data-page", 1);
+                }
+
+                $('.sobre-o-evento--texto p').toggle();
+            });
         });
 
     });
