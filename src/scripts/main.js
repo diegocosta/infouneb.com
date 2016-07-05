dc99ede14bc4c3e6735187d27e268866d0b13bb2
Diego Costa <diego@diegocosta.com.br>
2016-07-04 22:21:54 -0300

    Pequenos ajustes e criado sessão sobre o evento mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 (function($){
+    $(document).on('ready', function(){
 
+        $('.sobre-o-evento--button-continue').on('click', function(event){
+            event.preventDefault();
+
+            $('.sobre-o-evento--texto p').toggle();
+
+        });
+
+    });
 })(jQuery);
