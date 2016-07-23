8bd9a8fefcdf4593f6eaea1446873a5a795cbc0a
Diego Costa <diego@diegocosta.com.br>
2016-07-23 00:19:13 -0300

    Iniciado as abas de Speakers + Melhorado a exibição do About

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,29 +7,39 @@
         var MediaQueryBreakpoint = Foundation.MediaQuery.current;
 
         if(MediaQueryBreakpoint === 'small') {
-            $('.about--texto p:nth-child(3), .about--texto p:nth-child(4)').hide();
+            var about_hidden_content = $('.about--texto p').not('.about--texto p:nth-child(1)');
+
+            about_hidden_content.hide();
 
             $('.about--button-continue').each(function(index, item){
                 $this = $(this);
                 $this.css('display', 'block');
+
                 $this.on('click', function(event){
                     event.preventDefault();
-
-                    var btn = $(this);
-
-                    if(btn.attr('data-page') == 1){
-                        btn.text("Voltar");
-                        btn.attr("data-page", 2);
-                    }
-                    else {
-                        btn.text("Continuar Lendo");
-                        btn.attr("data-page", 1);
-                    }
-
-                    $('.about--texto p').toggle();
+                    about_hidden_content.toggle();
+                    $this.css('display', 'none');
                 });
+
             });
         }
+
+
+        $('.speakers').each(function(index, item){
+            var speakers = $(this);
+
+            speakers.find('[data-tab-content-hide]').hide();
+            speakers.find('.speakers--tab--list--item').find('.speakers--tab--list--link').on('click', function(event){
+                event.preventDefault();
+
+                var target = $(this).data('tab-link');
+
+                speakers.find('[data-tab-content-for]').hide();
+                speakers.find('[data-tab-content-for="' + target + '"]').show();
+                speakers.addClass('speakers__tab_' + target);
+            });
+
+        });
 
         $('.speakers .columns:last-child').addClass('end');
 
