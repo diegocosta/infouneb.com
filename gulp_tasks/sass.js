cefa9a1f9e88d791061ab1a4f3937514daef1430
Diego Costa <diego@diegocosta.com.br>
2016-08-11 11:01:40 -0300

    Finalizado o estilo do rodapé

--- gulp_tasks/sass.js
+++ gulp_tasks/sass.js
@@ -1,7 +1,7 @@
 module.exports = function(gulp, plugins, src, dest) {
 	return function(){
 		return gulp.src(src)
-			.pipe(plugins.sass())
+			.pipe(plugins.sass({discardUnused: {fontFace: false}}))
 			.pipe(gulp.dest(dest));
 	};
 };
